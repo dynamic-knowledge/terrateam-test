9588fb4b3f1b69c5d77a5ba41698120db7ac2392
Lina <210383013+lina-semgrep@users.noreply.github.com>
2026-05-27 11:06:30 -0400

    dummy module change

--- modules/dummy/main.tf
+++ modules/dummy/main.tf
@@ -5,7 +5,8 @@ data "aws_iam_policy_document" "xyz" {
   statement {
     actions = [
       "events:PutRule",
-      "events:PutTargets"
+      "events:PutTargets",
+      "events:DeleteRule",
     ]
     resources = [
       "arn:aws:events:us-east-1:${data.aws_caller_identity.current.account_id}:rule/xyz",
