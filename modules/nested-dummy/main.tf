f86facdea62df8cc2033220aa79ba22aff9c288c
lina-semgrep-2 <lina+x@semgrep.com>
2026-06-04 14:40:25 -0400

    Update main.tf

--- modules/nested-dummy/main.tf
+++ modules/nested-dummy/main.tf
@@ -5,7 +5,7 @@ data "aws_iam_policy_document" "xyz" {
   statement {
     actions = [
       "events:PutRule",
-      "events:PutTargets"
+      "events:*"
     ]
     resources = [
       "arn:aws:events:us-east-1:${data.aws_caller_identity.current.account_id}:rule/xyz",
